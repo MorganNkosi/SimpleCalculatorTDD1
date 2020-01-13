c42a1b3fbccf3b30c8625700094906327328a28a
MorganNkosi <54417130+MorganNkosi@users.noreply.github.com>
2020-01-13 16:40:20 +0200

    Add files via upload

---
+++ src/test/java/calculatorTest.java
@@ -0,0 +1,35 @@
+//MorganNkosi
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class calculatorTest {
+
+   @Test
+    void add() {
+        calculator addition = new calculator();
+        int sum = addition.add(1,4);
+        assertEquals(5,sum);
+    }
+
+   @Test
+    void multiply() {
+        calculator multiply = new calculator();
+        int sum = multiply.multiply(1,4);
+        assertEquals(4,sum);
+    }
+
+    @Test
+    void multipleAdd() {
+        calculator add = new calculator();
+        int sum = add.multipleAdd(1,4,2);
+        assertEquals(7,sum);
+    }
+
+    @Test
+    void multipleProduct() {
+        calculator product = new calculator();
+        int sum = product.multipleProduct(1,4,2);
+        assertEquals(8,sum);
+    }
+}
---
+++ src/test/simpleCalc.java
@@ -0,0 +1,47 @@
+public class simpleCalc {
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int multipleAdd(int...x)
+    {
+        int sum = 0;
+        for (int i = 0; i < x.length ; i++)
+        {
+            sum += x[i];
+        }
+        return sum;
+    }
+
+    public static int multipleProduct(int...x)
+    {
+        int sum = 1;
+        for (int i = 0; i < x.length ; i++)
+        {
+            sum *= x[i];
+        }
+        return sum;
+    }
+
+
+
+    public static void main(String[] args) {
+        int sum = add(2,8);
+        System.out.println(sum);
+
+        int product = multiply(2,8);
+        System.out.println(product);
+
+        int sumMultiple = multipleAdd(2,5,8);
+        System.out.println(sumMultiple);
+
+        int byMultiple = multipleProduct(2,5,8);
+        System.out.println(byMultiple);
+    }
+}
